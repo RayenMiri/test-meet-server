10ad2f58861c7f9237a97ba05bad817db3eb292b
RayenMiri <140018438+RayenMiri@users.noreply.github.com>
2025-03-30 00:29:29 +0000

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import { Server } from "socket.io";
 import app from "./app.js";
+import jwt from "jsonwebtoken";
 
 const server = http.createServer(app);
 const io = new Server(server, {
